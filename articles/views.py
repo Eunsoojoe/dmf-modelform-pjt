5bc2e1f9159c771adbfcea234a0be2aead77d415
Eunsoojoe <joeun2264@gmail.com>
2024-07-31 13:50:29 +0900

    Create(Ver.2) 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -13,17 +13,29 @@ def index(request):
     return render(request, 'index.html', context)
 
 def create(request):
+    # 모든 경우의 수
+    # GET : form을 만들어서 html 문서를 사용자에게 리턴 => 1~4번.
+    # POST invalid data(데이터 검증에 실패한 경우)
+    # POST valid data(데이터 검증에 성공한 경우) 
+
     # 기존
     # new/ => 빈 종이를 보여주는 기능
     # create/ => 사용자가 입력한 데이터를 저장하는 기능
     
-
-    # POST create/ => 사용자가 입력한 데이터를 저장
+    # 5. POST 요청 (데이터가 잘못 들어온 경우)
+    # 10. POST 요청 (데이터가 잘 들어온 경우)
     if request.method == 'POST':
+        # 6. 사용자가 입력한 데이터X(request.POST)을 담아서 form 생성
+        # 11. 사용자가 입력한 데이터O(request.POST)을 담아서 form 생성
         form = ArticleForm(request.POST)
 
+        # 7. form 검증 실패
+        # 12. form 검증 성공
         if form.is_valid():
+            # 13. form을 저장
             form.save()         # '검증'하여 데이터를 '한꺼번에' 저장
+            
+            # 14. index 페이지로 redirect
             return redirect('articles:index')
 
             # article = Article()
@@ -31,27 +43,31 @@ def create(request):
             # article.save()
             
         # 사용자가 데이터를 잘못 넣은 경우
-        else:    
+        # else:    
             # form = ArticleForm()
-            context = {
-                'form':form,
-            }
 
             # 너 게시물 그만 작성하고 index로 돌아갓 근데 사용자가 실수했을 수도 있잖아?
-            # return redirect('articles:index')    
+            # return redirect('articles:index')  
 
-            return render(request, 'create.html', context)
+            # context = {
+            #     'form':form,
+            # }
+            # return render(request, 'create.html', context)
 
-    # GET create/ => 빈 종이를 보여주는 기능
+    # 1. GET create/ => 빈 종이를 보여주는 기능
     else:
-        
+        # 2. 비어있는 form을 만들어서 
         form = ArticleForm()
 
-        context = {
-            'form' : form,
-        }
+    # 3. context dict에 비어있는 form을 담아서
+    # 8. context dict에 검증에 실패한 form을 담아서 
+    context = {
+        'form' : form,
+    }
 
-        return render(request, 'create.html', context)
+    # 4. create.html을 랜더링
+    # 9. create.html을 랜더링
+    return render(request, 'create.html', context)
     
     # forms.py에서 만든 ArticleForm 인스턴스화
     
