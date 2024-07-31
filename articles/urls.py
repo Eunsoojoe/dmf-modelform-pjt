2bcee871329bc9580a2b8dd7f3060a0d0d855478
Eunsoojoe <joeun2264@gmail.com>
2024-07-31 10:53:24 +0900

    Read(ALL) 기능 구현

---
+++ articles/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+
+urlpatterns = [
+
+    # 현재 articles 폴더 내에 있는 views.py를 import
+    path('', views.index, name='index'),
+    
+]
